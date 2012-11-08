ec613656d96687715a4d2f3d1c5f92ea658ce5b7
Tobias Vogt <tobias@code-x.de>
2012-11-08 14:48:42 +0100

    Added first tests

---
+++ src/app/code/community/FireGento/GermanSetup/Test/Model/TaxCalulation.php
@@ -0,0 +1,11 @@
+<?php
+
+class Firegento_Germansetup_Test_Model_TaxCalulation extends EcomDev_PHPUnit_Test_Case
+{
+
+    public function testTest()
+    {
+        $this->assertTrue( false );
+    }
+
+}
