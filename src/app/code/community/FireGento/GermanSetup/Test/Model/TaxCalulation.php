0d5b97cb9971cad5f1fca422ae457100ab086eab
Tobias Vogt <tobias@code-x.de>
2012-11-08 14:48:46 +0100

    Added first tests

--- src/app/code/community/FireGento/GermanSetup/Test/Model/TaxCalulation.php
+++ src/app/code/community/FireGento/GermanSetup/Test/Model/TaxCalulation.php
@@ -3,9 +3,36 @@
 class Firegento_Germansetup_Test_Model_TaxCalulation extends EcomDev_PHPUnit_Test_Case
 {
 
-    public function testTest()
+    /**
+     * @var Mage_Sales_Model_Quote
+     */
+    protected $_quote;
+
+    public function setUp()
     {
         $this->assertTrue( false );
     }
+
+    protected function setUpQuote()
+    {
+        $this->_quote = Mage::getModel('sales/quote');
+
+    }
+
+    public function testBruttopreis()
+    {
+        $this->fail();
+    }
+
+    public function testMwst()
+    {
+        $this->fail();
+    }
+
+    public function testSumme()
+    {
+        $this->fail();
+    }
+
 
 }
---
+++ src/app/code/community/FireGento/GermanSetup/Test/README.markdown
@@ -0,0 +1,4 @@
+Der Unit-Test basiert auf dem UnitTest Modul von EcomDev welches unter https://github.com/IvanChepurnyi/EcomDev_PHPUnit zu finden ist.
+Danach im Magento-Root ``phpunit UnitTests.php```aufrufen.
+
+(Eventuell vorher auf dem System PHPUnit installieren)
